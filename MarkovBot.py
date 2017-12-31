37f512b96bc47517da85a2ea344168b61d6acf08
Matthew Wolff <mm.wolff@chi-squared.org>
2017-12-30 23:37:42 -0600

    encapsulation

--- MarkovBot.py
+++ MarkovBot.py
@@ -34,7 +34,7 @@ class MarkovBot:
                  min_word_freq=3, seed=None, scrape_from=None):
         self.keys = api_key
         self.active = active_hours  # NOTE: limited use rn
-        self.api, self.me, self.handle, self.fancy_handle = self.verify(api_key, other_handle)
+        (self.api, self.me, self.handle, self.fancy_handle) = self.verify(other_handle)
         self.folder = "bot_files/%s/" % self.handle
         self.log = self.folder + "%s_log.txt" % self.handle
         self.corpus = self.folder + "%s.json" % self.handle
@@ -42,11 +42,9 @@ class MarkovBot:
         self.tokenizer = self.check_corpus(scrape_from, min_word_freq)
         self.chain_maker = Chain(self.handle, max_chains=max_chains, seed=seed)
 
-    def verify(self, api_key, handle):
+    def verify(self, handle):
         """
         Verifies that the user has valid credentials for accessing Tweepy API
-        :param api_key: a python dictionary object containing a "consumer_key","consumer_secret","access_token"
-            and "access_token_secret", in no particular order
         :param handle: the handle of the twitter user that the bot operator wishes to mimic
         :return: a 4-tuple of an API object, the bot's handle, the standardized handle of the other user, and the
             actual handle of the other user (can have uppercase letters)
@@ -55,22 +53,19 @@ class MarkovBot:
         thread = Thread(target=self.loading())  # lol
         thread.daemon = True  # kill this thread if program exits
         thread.start()
-        auth = tweepy.OAuthHandler(api_key["consumer_key"], api_key["consumer_secret"])
-        auth.set_access_token(api_key["access_token"], api_key["access_token_secret"])
-        api = tweepy.API(auth)
+
+        api = self.authorize()
         handle = handle.strip().lower()  # standardize name formatting for folder name
         try:
-            # test that API works
-            who_am_i = handle if handle in "test" else api.get_user(handle).screen_name
+            who_am_i = handle if handle in "test" else api.get_user(handle).screen_name  # test that API works
             me = api.me().screen_name
         except TweepError as e:
             err = e[:][0][0]["message"]
             raise ValueError("Awh dang dude, you gave me something bad: %s" % err)
-        thread.join()  # lol
 
-        print colors.white(" verified")
-        print colors.cyan("starting up bot ") + colors.white("@" + me) + colors.cyan(" as ") + colors.white(
-            "@" + who_am_i) + colors.cyan("!\n")
+        thread.join()  # lol
+        print colors.white(" verified\n") + colors.cyan("starting up bot ") + colors.white("@" + me) + colors.cyan(
+            " as ") + colors.white("@" + who_am_i) + colors.cyan("!\n")
         return api, me, handle, who_am_i  # api, the bot's name, the other user's name, full version of user's name
 
     def check_corpus(self, scrape_from_when, min_word_freq):
@@ -94,10 +89,10 @@ class MarkovBot:
             scraped = True
         if scrape_from_when and not scraped:  # they already had a corpus and need a special scrape
             scrape(self.handle, self.keys, start=scrape_from_when)
-        # always return the Tokenizer object
+
         tokenizer = Tokenizer(min_word_freq)
         tokenizer.generate(self.handle)
-        return tokenizer
+        return tokenizer  # always return the Tokenizer object
 
     def update(self, starting=None):
         """
@@ -105,10 +100,10 @@ class MarkovBot:
         the present. If no tweets were collected or not file was found, begins scraping from their join date.
         :param starting: The date to start scraping from (FORMAT: YYYY-MM-DD)
         """
-        if not starting:  # if not given, look from beginnign
+        if not starting:  # if not given, look from beginning
             starting = self.get_join_date()
         print colors.cyan("Updating corpus.json")
-        scrape(self.handle, start=starting)
+        scrape(self.handle, start=starting, api=self.keys)
 
     def chain(self, max_length=TWEET_MAX_LENGTH):
         """
@@ -122,7 +117,7 @@ class MarkovBot:
         chain_text = self.chain_maker.generate_chain(max_length)
         return colors.white("@" + self.fancy_handle) + colors.yellow(" says: ") + chain_text
 
-    def tweet_chain(self, max_length=TWEET_MAX_LENGTH, safe=False):
+    def tweet_chain(self, max_length=TWEET_MAX_LENGTH, safe=True):
         """
         Bot issues a tweet made by markov chaining
         :param max_length: the maximum number of characters allowed in the tweet - by default, max tweet length
@@ -162,6 +157,15 @@ class MarkovBot:
             raise ValueError(colors.red("Word frequency threshold must be greater than 0"))
         print colors.yellow("regenerating vocab with required min frequency at %s...\n" % new_min_frequency)
         self.tokenizer.generate(self.handle, new_min_frequency)
+
+    def authorize(self):
+        """
+        Uses keys to create an API accessor and returns it
+        :return: an object used to access the Tweepy API
+        """
+        auth = tweepy.OAuthHandler(self.keys["consumer_key"], self.keys["consumer_secret"])
+        auth.set_access_token(self.keys["access_token"], self.keys["access_token_secret"])
+        return tweepy.API(auth)
 
     def tweet(self, tweet=None, at=None):
         """
@@ -257,7 +261,7 @@ class MarkovBot:
         # too many characters (edge case)
         elif len(long_tweet) >= three_tweets_length:
             return 0, None
-            # 3 tweets
+        # 3 tweets
         elif len(long_tweet) > two_tweets_length:
             return 3, [handle + "(1/3) "
                        + long_tweet[:first_tweet_length],
@@ -273,7 +277,9 @@ class MarkovBot:
                        + long_tweet[first_tweet_length: len(long_tweet)]]
 
     def check_tweets(self):
-        """tweet upkeep multi-processing method"""
+        """
+        tweet upkeep multi-processing method, can be run continuously to check for anyone tweeting @ the bot
+        """
         print(colors.cyan("Beginning polling...\n"))
         while 1:
             try:
